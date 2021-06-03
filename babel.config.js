f4eef41826c0d31033d42f794f4f97595073aa55
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-06-03 14:02:24 -0300

    webpack.config.js added, babel configuration file and project general structure refactored

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
-export const presets = [
-    "@babel/preset-env",
-    "@babel/preset-react"
-];
+module.exports = {
+    presets: [
+        "@babel/preset-env",
+        "@babel/preset-react"
+    ]
+};
